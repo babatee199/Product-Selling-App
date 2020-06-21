a389f2f38a10668b9ca2cff607676b2b15751fb9
babatee199 <babatope_e@yahoo.com>
2020-06-21 03:48:48 +0100

    Doing seperation of concern by cutting off the routes

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const Thing = require('./models/thing');
+const stuffRouters = require('./routes/stuff');
 
 const app = express();
 
@@ -26,98 +27,7 @@ app.use((req, res, next) => {
 });
 app.use(bodyParser.json());
 
-//post middleware
-app.post('/api/stuff', (req, res, next) => {
-    const thing = new Thing({
-        title: req.body.title,
-        description: req.body.description,
-        imageUrl: req.body.imageUrl,
-        price: req.body.price,
-        userId: req.body.userId
-    });
-    thing.save().then(
-        () => {
-            res.status(201).json({
-                message: 'Post saved succesfully!'
-            })
-        }
-    ).catch( error => {
-        res.status(400).json({
-            error
-        })
-    })
-});
 
-//get middleware
-
-app.get('/api/stuff/:id', (req, res, next) => {
-    Thing.findOne({
-        _id: req.params.id
-    }).then(thing => {
-        res.status(200).json(thing);
-    }).catch(error => {
-        res.status(404).json({
-            error
-        })
-    })
-})
-
-
-app.get('/api/stuff', (req, res, next) => {
-    Thing.find().then(
-        things => {
-            res.status(200).json(things);
-        }
-    ).catch(
-        error => {
-            res.status(400).json({
-                error
-            })
-        }
-    )
-});
-
-
-//put middlewares
-app.put('/api/stuff/:id', (req, res, next) => {
-    const thing = new Thing({
-        _id: req.params.id,
-        title: req.body.title,
-        description: req.body.description,
-        imageUrl: req.body.imageUrl,
-        price: req.body.price,
-        userId: req.body.userId
-    });
-
-    Thing.updateOne({
-        _id: req.params.id
-    }, thing).then(
-        () => {
-            res.status(201).json({
-                message: 'Thing updated successfully!'
-            })
-        }
-    ).catch(error => {
-        res.status(400).json({
-            error
-        })
-    })
-});
-
-
-//middleware to delete a thing
-
-app.delete('/api/stuff/:id', (req, res, next) => {
-    Thing.deleteOne({_id: req.params.id }).then(
-        () => {
-            res.status(200).json({
-                message: `Item with id ${req.params.id} deleted successfully!`
-            })
-        }
-    ).catch(error => {
-        res.status(400).json({error})
-    })
-})
 
 
 module.exports = app;
